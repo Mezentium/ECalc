c7f3b073d7384b33a014133aa9fa157ddb4c29a6
gowkaizer <gowkaizer@yahoo.com>
2020-01-14 07:41:30 -0500

    Real initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>ECalcs, LLC</h1>
+            </header>
+            <div className="content">
+                <p>Providing energy calculations for a variety of situations backed by experience and professionalism.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Contact for more information</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
